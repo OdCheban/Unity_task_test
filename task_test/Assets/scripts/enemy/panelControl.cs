080ed7c4f6a1ce296ec06b8f6a4383b2e7c9f945
Oleg Cheban <odcheban@gmail.com>
2017-08-20 14:46:30 +0400

    #5 completed

---
+++ task_test/Assets/scripts/enemy/panelControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelControl : MonoBehaviour {
+    public GameObject prefab;
+    public float offsetSpawnEnemy;
+    public List<GameObject> massObj;
+
+
+    void SpawnEnemy()
+    {
+        float x = Random.Range(transform.position.x + offsetSpawnEnemy, transform.position.x - offsetSpawnEnemy);
+        float z = Random.Range(transform.position.z + offsetSpawnEnemy, transform.position.z - offsetSpawnEnemy);
+        GameObject unit = (GameObject)Instantiate(prefab, new Vector3(x,1, z), Quaternion.identity);
+        massObj.Add(unit);
+    }
+    void DestroyEnemy()
+    {
+        foreach (GameObject obj in massObj)
+            Destroy(obj);
+        massObj.Clear();
+    }
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Equals))
+        {
+            SpawnEnemy();
+        }
+        if(Input.GetKeyDown("-"))
+        {
+            DestroyEnemy();
+        }
+	}
+}
