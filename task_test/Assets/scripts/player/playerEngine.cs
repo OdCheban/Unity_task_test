67007da634031b7f59b5ac759a30288f208175cb
Oleg Cheban <odcheban@gmail.com>
2017-08-20 14:30:36 +0400

    #4 completed

--- task_test/Assets/scripts/player/playerEngine.cs
+++ task_test/Assets/scripts/player/playerEngine.cs
@@ -9,6 +9,49 @@ public class playerEngine : MonoBehaviour {
         float x = speed * Time.deltaTime * Input.GetAxis("Horizontal");
         float z = speed * Time.deltaTime * Input.GetAxis("Vertical");
 
+        RaycastHit hit = new RaycastHit();
+        if(Physics.Raycast(transform.position,transform.forward,out hit,0.5f))
+        {
+            if(hit.transform != transform)
+            {
+                if(z > 0)
+                {
+                    z = 0;
+                }
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.forward*-1, out hit, 0.5f))
+        {
+            if (hit.transform != transform)
+            {
+                if (z < 0)
+                {
+                    z = 0;
+                }
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.right, out hit, 0.5f))
+        {
+            if (hit.transform != transform)
+            {
+                if (x > 0)
+                {
+                    x = 0;
+                }
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.right * -1, out hit, 0.5f))
+        {
+            if (hit.transform != transform)
+            {
+                if (x < 0)
+                {
+                    x = 0;
+                }
+            }
+        }
+
+
         transform.Translate(x, 0, z);
     }
 }
