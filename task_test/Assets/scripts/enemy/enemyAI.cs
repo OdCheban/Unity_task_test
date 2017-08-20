338b51f35a5fbfc3a8a1f18df315ce87e55e0363
Oleg Cheban <odcheban@gmail.com>
2017-08-20 15:30:32 +0400

    #6 #7 completed

---
+++ task_test/Assets/scripts/enemy/enemyAI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour {
+    public float max_moveSpeed;
+    public float max_rotationSpeed;
+    public float mass;
+    public float friction;
+    [HideInInspector]
+    public Transform target;
+
+    public enum stateMove { Idle, Seek, Flee, Arrival, Wander, CollisionAvoidance, LeaderFollowing, Number }
+    public stateMove state = stateMove.Idle;
+
+    private Vector3 nowVelocity = Vector3.zero;
+
+	void Update () {
+		if(state != stateMove.Idle)
+        {
+            nowVelocity += GetVelocity()/mass;
+            nowVelocity -= nowVelocity * friction;
+            if(nowVelocity.magnitude > max_moveSpeed)
+            {
+                nowVelocity = nowVelocity.normalized * max_moveSpeed;
+            }
+            nowVelocity.y = 0;
+
+            transform.position += nowVelocity * Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(nowVelocity), max_rotationSpeed * Time.deltaTime);
+        }
+	}
+
+    Vector3 Seek(Transform targetEnd)
+    {
+        return ((targetEnd.position - transform.position).normalized * max_moveSpeed) - nowVelocity;
+    }
+
+    Vector3 GetVelocity()
+    {
+        if(state == stateMove.Seek)
+        {
+            return Seek(target);
+        }
+        return Vector3.zero;
+    }
+
+}
--- task_test/Assets/scripts/enemy/panelControl.cs
+++ task_test/Assets/scripts/enemy/panelControl.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class panelControl : MonoBehaviour {
     public GameObject prefab;
     public float offsetSpawnEnemy;
     public List<GameObject> massObj;
+    public Text stateNowUI;
+    public enum stateMove { Idle,Seek,Flee,Arrival,Wander,CollisionAvoidance,LeaderFollowing,Number }
+    public stateMove state = stateMove.Idle;
 
 
     void SpawnEnemy()
@@ -13,6 +17,7 @@ public class panelControl : MonoBehaviour {
         float x = Random.Range(transform.position.x + offsetSpawnEnemy, transform.position.x - offsetSpawnEnemy);
         float z = Random.Range(transform.position.z + offsetSpawnEnemy, transform.position.z - offsetSpawnEnemy);
         GameObject unit = (GameObject)Instantiate(prefab, new Vector3(x,1, z), Quaternion.identity);
+        unit.GetComponent<enemyAI>().target = gameObject.transform;
         massObj.Add(unit);
     }
     void DestroyEnemy()
@@ -20,6 +25,11 @@ public class panelControl : MonoBehaviour {
         foreach (GameObject obj in massObj)
             Destroy(obj);
         massObj.Clear();
+    }
+
+    void getData(int q)
+    {
+
     }
 
 	void Update () {
@@ -31,5 +41,21 @@ public class panelControl : MonoBehaviour {
         {
             DestroyEnemy();
         }
+        for(int i = 0; i < (int)stateMove.Number;i++)
+        {
+            if (Input.GetKeyDown(i.ToString()))
+            {
+                state = (stateMove)i;
+                stateNowUI.text = stateMove.GetName(typeof(stateMove), i);
+            }
+
+            for(int j = 0; j < massObj.Count;j++)
+            {
+                massObj[j].GetComponent<enemyAI>().state = (enemyAI.stateMove)state;
+            }
+
+        }
+
+
 	}
 }
