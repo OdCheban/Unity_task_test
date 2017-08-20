779dce556342144bf4b05f6f30ab0ea6de5bb2d7
Oleg Cheban <odcheban@gmail.com>
2017-08-20 14:20:59 +0400

    #3 #2 #1 completed

---
+++ task_test/Assets/scripts/player/cameraPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraPlayer : MonoBehaviour {
+    private Transform target;
+    private Vector3 cameraPos;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cameraPos = new Vector3(target.position.x,transform.position.y,target.position.z);
+        transform.position = Vector3.Lerp(transform.position,cameraPos,Time.deltaTime*speed);
+	}
+}
---
+++ task_test/Assets/scripts/player/playerEngine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerEngine : MonoBehaviour {
+    public float speed;
+
+	void Update () {
+        float x = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float z = speed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        transform.Translate(x, 0, z);
+    }
+}
